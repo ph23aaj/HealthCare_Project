1be249c345af53d98461a82ef100930b82890238
Pasha Hosseini <ph23aaj@herts.ac.uk>
2025-12-21 16:16:42 +0000

    Completed AppointmentType Enum

--- src/AppointmentType.java
+++ src/AppointmentType.java
@@ -1,7 +1,18 @@
 public enum AppointmentType {
 
-    ROUTINECONSULTATION(" Routine Consultation"), VACCINATIO(" Vaccination"),
-    ;
+    ROUTINE(" Routine Consultation"), VACCINATION(" Vaccination"),
+    FOLLOWUP(" Follow-up"), URGENT(" Urgent Consultation"), SPECIALIST( " Specialist Consultation"),
+    EMERGENCY(" Emergency"), HEALTHCHECK(" Health Check");
     private String type;
 
+    private AppointmentType (String ty){
+        type = ty;
+    }
+
+    @Override
+    public String toString() {
+        return "AppointmentType{" +
+                "type='" + type + '\'' +
+                '}';
+    }
 }
